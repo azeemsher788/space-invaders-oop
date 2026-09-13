6e0294b83197ba94875d20d6616823ec3cf7787a
azeemsher788 <azeemsher788@users.noreply.github.com>
2026-09-13 19:24:16 +0500

    style: fix isort formatting in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import os
+
 import pygame
 import pytest
 
 # Initialize pygame headless for tests
 os.environ["SDL_VIDEODRIVER"] = "dummy"
+
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_pygame():
